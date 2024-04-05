35d7c166ab8699fbdeffb4361d9344bca4f8a7da
Lucas Lacerda <lucas.ls.lacerda@elo.com.br>
2024-04-05 07:45:55 -0300

    fix: add prefix on plugin

--- packages/ui/src/tw-plugin/plugin.ts
+++ packages/ui/src/tw-plugin/plugin.ts
@@ -2,23 +2,21 @@ import plugin from 'tailwindcss/plugin';
 import { SPACING, COLORS, FONT_WEIGHT, FONT_FAMILY, FONT_SIZE } from "@eloquent/tokens";
 
 export const eloquentPlugin = plugin(({ addUtilities }) => {
-  const newUtilities = {
-    '.text': Object.entries(FONT_SIZE).reduce((acc: {[key: string]: {fontSize: string}}, [name, value]) => {
-      acc[`.text-${name}`] = { fontSize: value };
+  const newUtilities: Record<string, any> = {
+    ...Object.entries(FONT_SIZE).reduce((acc: Record<string, any>, [name, value]) => {
+      acc[`.orb-text-${name}`] = { fontSize: value };
       return acc;
     }, {}),
-    '.font': {
-      ...Object.entries(FONT_FAMILY).reduce((acc: {[key: string]: {fontFamily: string}}, [name, value]) => {
-        acc[`.font-${name}`] = { fontFamily: value };
-        return acc;
-      }, {}),
-      ...Object.entries(FONT_WEIGHT).reduce((acc: {[key: string]: {fontWeight: string}}, [name, value]) => {
-        acc[`.font-${name}`] = { fontWeight: value };
-        return acc;
-      }, {}),
-    },
-    '.spacing': Object.entries(SPACING).reduce((acc: {[key: string]: {margin: string}}, [name, value]) => {
-      acc[`.spacing-${name}`] = { margin: value };
+    ...Object.entries(FONT_FAMILY).reduce((acc: Record<string, any>, [name, value]) => {
+      acc[`.orb-font-${name}`] = { fontFamily: value };
+      return acc;
+    }, {}),
+    ...Object.entries(FONT_WEIGHT).reduce((acc: Record<string, any>, [name, value]) => {
+      acc[`.orb-font-${name}`] = { fontWeight: value };
+      return acc;
+    }, {}),
+    ...Object.entries(SPACING).reduce((acc: Record<string, any>, [name, value]) => {
+      acc[`.orb-spacing-${name}`] = { margin: value };
       return acc;
     }, {}),
   };
@@ -28,7 +26,7 @@ export const eloquentPlugin = plugin(({ addUtilities }) => {
   return {
     theme: {
       extend: {
-        colors: Object.entries(COLORS).reduce((acc: {[key: string]: any}, [name, value]) => {
+        colors: Object.entries(COLORS).reduce((acc: Record<string, any>, [name, value]) => {
           if (typeof value === 'string') {
             acc[`${name}`] = value;
           } else {
