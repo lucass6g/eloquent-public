e6007d7a4cc60e72a53ebc9f35f73c06a4b0307b
Lucas Lacerda <lucas.ls.lacerda@elo.com.br>
2024-04-05 07:22:57 -0300

    fix: fix plugin

--- packages/ui/src/tw-plugin/plugin.ts
+++ packages/ui/src/tw-plugin/plugin.ts
@@ -1,98 +1,51 @@
 import plugin from 'tailwindcss/plugin';
 import { SPACING, COLORS, FONT_WEIGHT, FONT_FAMILY, FONT_SIZE } from "@eloquent/tokens";
 
-export const eloquentPlugin = plugin(function({ addBase }) {
-  const newTheme = {
-    extend: {
-      margin: SPACING,
-      padding: SPACING,
-      spacing: SPACING,
-      gap:  SPACING,
-      screens: {
-        xsm: "500px",
-        sm: "640px",
-        md: "768px",
-        lg: "1024px",
-        mobile: {
-          min: "0",
-          max: "599px",
-        },
-        tablet: {
-          min: "600px",
-          max: "959px",
-        },
-        laptop: {
-          min: "960px",
-          max: "1271px",
-        },
-        desktop: {
-          min: "1272px",
-          max: "1769px",
-        },
-        full: "1680px",
-        print: { raw: "print" },
-      },
-      fontSize: FONT_SIZE,
-      fontWeight: FONT_WEIGHT,
-      fontFamily: FONT_FAMILY,
-      letterSpacing: {
-        small: "-1px",
-        medium: "-2px",
-        large: "-3px",
-      },
-      lineHeight: {
-        default: "100%",
-        tiny: "115%",
-        medium: "120%",
-        large: "133%",
-        distant: "150%",
-        superdistant: "170%",
-      },
-      borderWidth: {
-        none: "0px",
-        hairline: "1px",
-        thin: "2px",
-        thick: "4px",
-        heavy: "8px",
-      },
-      transitionDelay: {
-        DEFAULT: "",
-      },
-      transitionTimingFunction: {
-        DEFAULT: "cubic-bezier(0.48, 0, 0.48, 1)",
-      },
-      transitionDuration: {
-        DEFAULT: "0.275s",
-      },
-      borderRadius: {
-        none: "0px",
-        pill: "500px",
-        circle: "50%",
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
-      },
-      opacity: {
-        0: "0",
-        1: "1",
-        semiopaque: "0.8",
-        intense: "0.64",
-        medium: "0.32",
-        light: "0.16",
-        semitransparent: "0.08",
-      },
-      boxShadow: {
-        "level-1": "0 4px 8px rgba(69, 73, 84, 0.08)",
-        "level-2": "0 8px 24px rgba(69, 73, 84, 0.08)",
-        "level-3": "0 16px 32px rgba(69, 73, 84, 0.08)",
-        "level-4": "0 16px 48px rgba(69, 73, 84, 0.08)",
-      },
-      colors: COLORS,
-      aspectRatio: {
-        "4/3": "4 / 3",
+export const eloquentPlugin = plugin(({ addUtilities }) => {
+  const newUtilities = {
+    '.text': Object.entries(FONT_SIZE).reduce((acc: {[key: string]: {fontSize: string}}, [name, value]) => {
+      acc[`.text-${name}`] = { fontSize: value };
+      return acc;
+    }, {}),
+    '.font': {
+      ...Object.entries(FONT_FAMILY).reduce((acc: {[key: string]: {fontFamily: string}}, [name, value]) => {
+        acc[`.font-${name}`] = { fontFamily: value };
+        return acc;
+      }, {}),
+      ...Object.entries(FONT_WEIGHT).reduce((acc: {[key: string]: {fontWeight: string}}, [name, value]) => {
+        acc[`.font-${name}`] = { fontWeight: value };
+        return acc;
+      }, {}),
+    },
+    '.spacing': Object.entries(SPACING).reduce((acc: {[key: string]: {margin: string}}, [name, value]) => {
+      acc[`.spacing-${name}`] = { margin: value };
+      return acc;
+    }, {}),
+  };
+
+  addUtilities(newUtilities);
+
+  return {
+    theme: {
+      extend: {
+        colors: Object.entries(COLORS).reduce((acc: {[key: string]: any}, [name, value]) => {
+          if (typeof value === 'string') {
+            acc[`${name}`] = value;
+          } else {
+            acc[`${name}`] = {};
+            Object.entries(value).forEach(([key, val]) => {
+              if (typeof val === 'string') {
+                acc[`${name}`][`${key}`] = val;
+              }
+            });
+          }
+          return acc;
+        }, {}),
+        spacing: SPACING,
+        fontSize: FONT_SIZE,
+        fontWeight: FONT_WEIGHT,
+        fontFamily: FONT_FAMILY,
       },
     },
   };
-
-  addBase(newTheme);
 });
