bde2817b873d17c5f77c2f33fbcb0fffed3864c3
Lucas Lacerda <lucas.ls.lacerda@elo.com.br>
2024-04-07 10:29:07 -0300

    feat: add preset tailwind on tokens

---
+++ packages/tokens/preset/tailwind.config.ts
@@ -0,0 +1,13 @@
+import {FONT_SIZE, FONT_FAMILY, FONT_WEIGHT, COLORS, SPACING} from "../index"
+
+module.exports = {
+    theme: {
+        extend: {
+            colors: COLORS,
+            fontSize: FONT_SIZE,
+            fontFamily: FONT_FAMILY,
+            fontWeight: FONT_WEIGHT,
+            spacing: SPACING,
+        }
+    },
+}
