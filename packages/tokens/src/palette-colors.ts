cc3daf1f380f5c9a3aec0a558b24b2d9705597a2
Lucas Lacerda <lucas.ls.lacerda@elo.com.br>
2024-04-20 12:10:38 -0300

    feat: adicionado novas cores no storybook de exemplo

--- packages/tokens/src/palette-colors.ts
+++ packages/tokens/src/palette-colors.ts
@@ -1,22 +1,41 @@
-import { ALERT, BLUE_SKY, GREEN_FOREST, GREEN_SEA, INFORMATIVE, NEGATIVE, NEUTRAL, POSITIVE, RED_LAND, YELLOW_SUN } from "@/colors"
+import {
+	ALERT,
+	BLUE_SKY,
+	GREEN_FOREST,
+	GREEN_SEA,
+	INFORMATIVE,
+	NEGATIVE,
+	NEUTRAL,
+	POSITIVE,
+	RED_LAND,
+	YELLOW_SUN,
+} from "@/colors"
 
 export const PALETTE_COLORS = {
-	neutral:        NEUTRAL,
-	positive:       POSITIVE,
-	informative:    INFORMATIVE,
-	negative:       NEGATIVE,
-	alert:          ALERT,
-	"red-land":     RED_LAND,
-	"green-forest": GREEN_FOREST,
-	"green-sea":    GREEN_SEA,
-	"blue-sky":     BLUE_SKY,
-	"yellow-sun":   YELLOW_SUN,
-	"brand": {
-		forest: GREEN_FOREST['500'],
+	neutral:     NEUTRAL,
+	positive:    POSITIVE,
+	informative: INFORMATIVE,
+	negative:    NEGATIVE,
+	alert:       ALERT,
+	green:       {
+		forest: GREEN_FOREST,
+		sea:    GREEN_SEA,
+	},
+	red:         {
+		land: RED_LAND,
+	},
+	blue:        {
+		sky: BLUE_SKY,
+	},
+	yellow:      {
+		sun: YELLOW_SUN,
+	},
+	brand:       {
 		sea:    GREEN_SEA['600'],
 		sky:    BLUE_SKY['500'],
 		sun:    YELLOW_SUN['600'],
 		land:   RED_LAND['600'],
-	}
-};
+		forest: GREEN_FOREST['500'],
+	},
+}
 
