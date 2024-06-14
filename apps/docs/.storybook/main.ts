d237d2d076936ea82b696414e04d09008a968abc
Lucas Lacerda <lucas.ls.lacerda@elo.com.br>
2024-06-14 08:21:34 -0300

    feat: adicionado addons de tema e logo elo

--- apps/docs/.storybook/main.ts
+++ apps/docs/.storybook/main.ts
@@ -19,8 +19,9 @@ const config: StorybookConfig = {
     getAbsolutePath("@storybook/addon-essentials"),
     getAbsolutePath("@storybook/addon-onboarding"),
     getAbsolutePath("@storybook/addon-interactions"),
-    getAbsolutePath("@storybook/addon-mdx-gfm"),
-      getAbsolutePath("@storybook/addon-a11y")
+    getAbsolutePath("@storybook/addon-a11y"),
+    getAbsolutePath("@storybook/addon-themes"),
+    getAbsolutePath("storybook-dark-mode")
   ],
 
   core: {},
---
+++ apps/docs/.storybook/themes-Storybook-UI.ts
@@ -0,0 +1,43 @@
+
+export const darkUIStorybook = {
+    colorPrimary: "hsl(210 40% 98%)",
+    colorSecondary: "hsl(217.2 32.6% 17.5%)",
+
+    // UI
+    appBg: "hsl(222.2 84% 4.9%)",
+    appContentBg: "hsl(222.2 84% 4.9%)",
+    appBorderColor: "hsl(217.2 32.6% 17.5%)",
+    appBorderRadius: ".4rem",
+
+    // Typography
+    // fontBase: "",
+    // fontCode: '',
+
+    // Text colors
+    textColor: "hsl(210 40% 98%)",
+    textInverseColor: "hsl(217.2 32.6% 17.5%)",
+
+    // Toolbar default and active colors
+    barTextColor: "hsl(210 40% 98%)",
+    barSelectedColor: "hsl(210 40% 98%)",
+    barBg: "hsl(222.2 84% 4.9%)",
+
+    // Form colo,
+    inputBg: "hsl(217.2 32.6% 17.5%)",
+    inputBorder: "hsl(217.2 32.6% 17.5%)",
+    inputTextColor: "hsl(210 40% 98%)",
+    inputBorderRadius: "0.4rem",
+
+    // Brand
+    brandImage: "https://media-linkedoffers.elo.com.br/cms-campaigns/assets/logo_branca_rebranding_22a08bce11.png",
+}
+
+export const lightUIStorybook = {
+    brandImage: "https://media-linkedoffers.elo.com.br/cms-campaigns/assets/logo_preta_rebranding_09e841e7c5.png",
+}
+
+export const commonTheme = {
+    brandTitle: "Eloquent",
+    brandUrl: "https://designsystem.elo.corp/docs",
+    brandTarget: "_blank",
+}
