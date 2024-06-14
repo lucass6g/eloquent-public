f74906bda1061f66a9e523e2fce8b4fdba7e96da
Lucas Lacerda <lucas.ls.lacerda@elo.com.br>
2024-06-14 08:21:34 -0300

    feat: adicionado addons de tema e logo elo

--- apps/docs/.storybook/preview.ts
+++ apps/docs/.storybook/preview.ts
@@ -1,9 +1,22 @@
 import "../../../packages/ui/styles/global.css"
 import {Preview} from '@storybook/react'
+import { themes } from '@storybook/theming';
+import {darkUIStorybook, lightUIStorybook, commonTheme} from "./themes-Storybook-UI"
 
 const preview: Preview = {
     parameters: {
         actions: {argTypesRegex: "^on[A-Z].*"},
+        darkMode: {
+            classTarget: "html",
+            stylePreview: true,
+            darkClass: "dark",
+            lightClass: "light",
+            // Override the default dark theme
+            dark: { ...themes.dark, ...darkUIStorybook, ...commonTheme},
+            // Override the default light theme
+            light: { ...themes.normal, ...lightUIStorybook, ...commonTheme},
+            current: "light",
+        },
         controls: {
             matchers: {
                 color: /(background|color)$/i,
