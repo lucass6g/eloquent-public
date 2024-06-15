8b1cf764da26e180772036fe278ca9ceea5e410d
Lucas Lacerda <lucas.ls.lacerda@elo.com.br>
2024-06-15 14:34:15 -0300

    feat: add alert story

--- packages/ui/src/components/alert.tsx
+++ packages/ui/src/components/alert.tsx
@@ -3,14 +3,14 @@ import {cva, type VariantProps} from "class-variance-authority"
 
 import {cn} from "../lib/utils"
 
-const alertVariants = cva(
-    "relative w-full rounded-lg border px-4 py-3 text-sm [&>svg+div]:translate-y-[-3px] [&>svg]:absolute [&>svg]:left-4 [&>svg]:top-4 [&>svg]:text-foreground [&>svg~*]:pl-7",
+export const alertVariants = cva(
+    "relative w-full rounded-lg border px-4 py-3 text-sm [&>svg+div]:translate-y-[-3px] [&>svg]:absolute [&>svg]:left-4 [&>svg]:stroke-foreground [&>svg~*]:pl-7",
     {
         variants: {
             variant: {
                 default: "bg-background text-foreground",
                 destructive:
-                    "border-destructive/50 text-destructive dark:border-destructive [&>svg]:text-destructive",
+                    "border-destructive/50 text-destructive border-destructive [&>svg]:stroke-destructive *:text-destructive",
             },
         },
         defaultVariants: {
