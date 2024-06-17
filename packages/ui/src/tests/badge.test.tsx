860d438252dd0be4c23bec0b1bbd4408a19f838e
Lorrany Abrantes <lorrany.abrantes@elo.com.br>
2024-06-17 18:29:05 -0300

    test: badge component

---
+++ packages/ui/src/tests/badge.test.tsx
@@ -0,0 +1,30 @@
+import { render } from "@testing-library/react";
+import { Badge } from "..";
+
+describe("Badge", () => {
+    describe("should match the snapshot", () => {
+        it("default", () => {
+            const component = render(<Badge>badge</Badge>);
+
+            expect(component.baseElement).toMatchSnapshot()
+        })
+
+        it("secondary", () => {
+            const component = render(<Badge variant="secondary">badge</Badge>);
+
+            expect(component.baseElement).toMatchSnapshot()
+        })
+
+        it("outline", () => {
+            const component = render(<Badge variant="outline">badge</Badge>);
+
+            expect(component.baseElement).toMatchSnapshot()
+        })
+
+        it("destructive", () => {
+            const component = render(<Badge variant="destructive">badge</Badge>);
+
+            expect(component.baseElement).toMatchSnapshot()
+        })
+    })
+}); 
