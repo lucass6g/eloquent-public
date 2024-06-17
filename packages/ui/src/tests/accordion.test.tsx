b432ffc335446bda1fa5fd248a5f1f6b5ea3cd60
Lorrany Abrantes <lorrany.abrantes@elo.com.br>
2024-06-17 15:57:59 -0300

    test: accordion component

--- packages/ui/src/tests/accordion.test.tsx
+++ packages/ui/src/tests/accordion.test.tsx
@@ -1,16 +1,33 @@
-import { render } from "@testing-library/react";
-import { describe, expect, it } from "vitest";
+import { render, waitFor } from "@testing-library/react";
 import { Accordion } from "..";
 
 describe("Accordion", () => {
     it("should match the snapshot", () => {
         const component = render(<Accordion.Root type="single" collapsible>
             <Accordion.Item value="item-1">
-                <Accordion.Trigger>teste</Accordion.Trigger>
-                <Accordion.Content>teste</Accordion.Content>
+                <Accordion.Trigger>trigger</Accordion.Trigger>
+                <Accordion.Content>content</Accordion.Content>
             </Accordion.Item>
         </Accordion.Root>);
 
         expect(component.baseElement).toMatchSnapshot()
+    })
+
+    it("should open and close correctly", async () => {
+        const component = render(<Accordion.Root type="single" collapsible>
+            <Accordion.Item value="item-1">
+                <Accordion.Trigger>trigger</Accordion.Trigger>
+                <Accordion.Content>content</Accordion.Content>
+            </Accordion.Item>
+        </Accordion.Root>);
+
+        const trigger = component.getByRole('button', { name: /trigger/i })
+        await waitFor(() => trigger.click())
+
+        const content = component.queryByText(/content/i)
+        expect(content).toBeInTheDocument()
+
+        await waitFor(() => trigger.click())
+        expect(content).not.toBeInTheDocument()
     })
 }); 
