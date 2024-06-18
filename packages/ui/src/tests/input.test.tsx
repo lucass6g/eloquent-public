f2cc3673c5578b18a0a1b8c863122117423e219e
Lorrany Abrantes <lorrany.abrantes@elo.com.br>
2024-06-18 11:50:30 -0300

    test: input component

---
+++ packages/ui/src/tests/input.test.tsx
@@ -0,0 +1,46 @@
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import { Input } from "..";
+
+describe("Input", () => {
+    it("should match the snapshot", () => {
+        const component = render(<Input
+            type="email"
+            placeholder="email"
+        />);
+
+        expect(component.baseElement).toMatchSnapshot()
+    })
+
+    it("should be filled out correctly", async () => {
+        const stateFn = vi.fn()
+
+        render(<Input
+            type="email"
+            placeholder="email"
+            onChange={stateFn}
+        />);
+
+        const input = screen.getByPlaceholderText('email')
+        fireEvent.change(input, { target: { value: 'placeholder@elo.com' } })
+
+        const filledInput = screen.getByDisplayValue('placeholder@elo.com')
+        expect(filledInput).toBeInTheDocument()
+
+        expect(stateFn).toHaveBeenCalled()
+    })
+
+    it("should be disabled", async () => {
+        const stateFn = vi.fn()
+
+        render(<Input
+            type="email"
+            placeholder="email"
+            onChange={stateFn}
+            disabled
+        />);
+
+        const input = screen.getByPlaceholderText('email')
+
+        expect(input).toBeDisabled();
+    })
+}); 
