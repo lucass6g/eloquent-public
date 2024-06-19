45d646b8145ffd9357c4eb98c286d743fae09caa
Lorrany Abrantes <lorrany.abrantes@elo.com.br>
2024-06-19 14:46:09 -0300

    chore: use @testing-library/jest-dom/vitest

--- packages/ui/setupTests.ts
+++ packages/ui/setupTests.ts
@@ -1,24 +1,24 @@
-import "@testing-library/jest-dom";
+import "@testing-library/jest-dom/vitest";
 import { cleanup } from "@testing-library/react";
 import { afterEach } from "vitest";
 
 global.ResizeObserver = require("resize-observer-polyfill");
 
 afterEach(() => {
-	cleanup();
+  cleanup();
 });
 
 class MockPointerEvent extends Event {
-	button: number;
-	ctrlKey: boolean;
-	pointerType: string;
+  button: number;
+  ctrlKey: boolean;
+  pointerType: string;
 
-	constructor(type: string, props: PointerEventInit) {
-		super(type, props);
-		this.button = props.button || 0;
-		this.ctrlKey = props.ctrlKey || false;
-		this.pointerType = props.pointerType || 'mouse';
-	}
+  constructor(type: string, props: PointerEventInit) {
+    super(type, props);
+    this.button = props.button || 0;
+    this.ctrlKey = props.ctrlKey || false;
+    this.pointerType = props.pointerType || "mouse";
+  }
 }
 
 window.PointerEvent = MockPointerEvent as any;
