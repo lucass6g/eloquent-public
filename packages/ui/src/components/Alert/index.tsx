8d965dad797546efe2c1dfa28f0ba798502d8d50
Lucas Lacerda <lucas.ls.lacerda@elo.com.br>
2024-07-02 16:40:53 -0300

    refactor: corrigido export do button variants

---
+++ packages/ui/src/components/Alert/index.tsx
@@ -0,0 +1,63 @@
+import * as React from "react";
+import { cva, type VariantProps } from "class-variance-authority";
+
+import { cn } from "@eloquent/styles";
+
+export const alertVariants = cva(
+    "relative w-full rounded-lg border px-4 py-3 text-sm [&>svg]:stroke-primary-foreground [&>svg+div]:translate-y-[-3px] [&>svg]:absolute [&>svg]:left-4 [&>svg]:stroke-foreground [&>svg~*]:pl-7",
+    {
+        variants: {
+            variant: {
+                default: "bg-background text-foreground",
+                destructive:
+                    " bg-background border-destructive/50 text-destructive border-destructive [&>svg]:stroke-destructive *:text-destructive",
+            },
+        },
+        defaultVariants: {
+            variant: "default",
+        },
+    }
+);
+
+const RootAlert = React.forwardRef<
+    HTMLDivElement,
+    React.HTMLAttributes<HTMLDivElement> & VariantProps<typeof alertVariants>
+>(({ className, variant, ...props }, ref) => (
+    <div
+        ref={ref}
+role="alert"
+className={cn(alertVariants({ variant }), className)}
+{...props}
+/>
+));
+RootAlert.displayName = "Alert";
+
+const AlertTitle = React.forwardRef<
+    HTMLParagraphElement,
+    React.HTMLAttributes<HTMLHeadingElement>
+>(({ className, ...props }, ref) => (
+    <h5
+        ref={ref}
+className={cn("mb-1 font-medium leading-none tracking-tight", className)}
+{...props}
+/>
+));
+AlertTitle.displayName = "AlertTitle";
+
+const AlertDescription = React.forwardRef<
+    HTMLParagraphElement,
+    React.HTMLAttributes<HTMLParagraphElement>
+>(({ className, ...props }, ref) => (
+    <div
+        ref={ref}
+className={cn("text-sm [&_p]:leading-relaxed", className)}
+{...props}
+/>
+));
+AlertDescription.displayName = "AlertDescription";
+
+export const Alert = {
+    Root: RootAlert,
+    Title: AlertTitle,
+    Description: AlertDescription,
+};
--- packages/ui/src/components/AlertDialog/index.ts
+++
@@ -1,2 +0,0 @@
-"use client"
-export  {AlertDialog} from "./alert-dialog"
