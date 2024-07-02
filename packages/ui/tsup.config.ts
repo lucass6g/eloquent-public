39d68f158ce862b5eef930d26fcbfad54e6b3926
Lucas Lacerda <lucas.ls.lacerda@elo.com.br>
2024-07-02 10:11:05 -0300

    chore: teste de novo export

--- packages/ui/tsup.config.ts
+++ packages/ui/tsup.config.ts
@@ -1,6 +1,6 @@
 import {defineConfig} from 'tsup';
 
-const useClientRegex = /['"]use client['"]\s?;/i;
+// const useClientRegex = /['"]use client['"]\s?;/i;
 
 export default defineConfig({
     name: "@eloquent/ui",
@@ -13,21 +13,21 @@ export default defineConfig({
     dts: true,
     target: "es2022",
     silent: false,
-    esbuildPlugins: [
-        {
-            name: "use-client",
-            setup(build) {
-                build.onEnd((result) => {
-                    result.outputFiles?.forEach((file) => {
-                        if (file.text.match(useClientRegex)) {
-                            Object.defineProperty(file, "text", {
-                                value:
-                                    '"use client";\n' + file.text.replace(useClientRegex, ""),
-                            });
-                        }
-                    });
-                });
-            },
-        },
-    ],
+    // esbuildPlugins: [
+    //     {
+    //         name: "use-client",
+    //         setup(build) {
+    //             build.onEnd((result) => {
+    //                 result.outputFiles?.forEach((file) => {
+    //                     if (file.text.match(useClientRegex)) {
+    //                         Object.defineProperty(file, "text", {
+    //                             value:
+    //                                 '"use client";\n' + file.text.replace(useClientRegex, ""),
+    //                         });
+    //                     }
+    //                 });
+    //             });
+    //         },
+    //     },
+    // ],
 })
