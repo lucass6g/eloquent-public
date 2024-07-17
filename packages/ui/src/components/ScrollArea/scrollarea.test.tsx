568ccf0a7c01c744309ad12803b1fd535a1228f8
elo1230027 <lucas.oliveira@elo.com.br>
2024-07-17 09:56:54 -0300

    fix: testes ScrollArea

---
+++ packages/ui/src/components/ScrollArea/scrollarea.test.tsx
@@ -0,0 +1,38 @@
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import { ScrollArea } from '.'
+
+describe('ScrollArea Component', () => {
+    it('renders children inside RootScrollArea', () => {
+        render(
+            <ScrollArea.Root>
+                <div>Test Child</div>
+            </ScrollArea.Root>
+        )
+        expect(screen.getByText('Test Child')).toBeInTheDocument()
+    })
+
+    describe("should match the snapshot", () => {
+        it("horizontal", () => {
+            const component = render(
+                <ScrollArea.Root>
+                    <div>Test Child</div>
+                    <ScrollArea.ScrollBar orientation="horizontal" />
+                </ScrollArea.Root>
+            );
+
+            expect(component.baseElement).toMatchSnapshot();
+        });
+
+        it("vertical", () => {
+            const component = render(
+                <ScrollArea.Root>
+                    <div>Test Child</div>
+                    <ScrollArea.ScrollBar orientation="vertical" />
+                </ScrollArea.Root>
+            );
+
+            expect(component.baseElement).toMatchSnapshot();
+        });
+    });
+})
