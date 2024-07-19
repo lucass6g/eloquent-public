424858376c16718819e6241642a866a0a938b019
Thiago <thiago.ribeiro@elo.com.br>
2024-07-19 10:19:05 -0300

    feat: added stories and tests

--- packages/ui/src/components/Label/index.tsx
+++ packages/ui/src/components/Label/index.tsx
@@ -7,7 +7,7 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@eloquent/styles"
 
 const labelVariants = cva(
-  "text-sm text-foreground font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+  "text-sm text-foreground font-normal text-neutral-500 leading-4 peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
 )
 
 const Label = React.forwardRef<
