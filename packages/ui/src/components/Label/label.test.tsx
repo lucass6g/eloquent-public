17a83f4e5d6c7fc67a480286e3b00de7342551ef
Thiago <thiago.ribeiro@elo.com.br>
2024-07-22 12:27:13 -0300

    fix: adjusting label

--- packages/ui/src/components/Label/label.test.tsx
+++ packages/ui/src/components/Label/label.test.tsx
@@ -1,5 +1,5 @@
-import { render, screen } from '@testing-library/react';
 import {it, describe, expect} from "vitest"
+import { render, screen } from '@testing-library/react';
 import { Label } from '.';
 
 describe("Label", () => {
---
+++ packages/ui/src/components/Label/variants.ts
@@ -0,0 +1,5 @@
+import { tv } from "tailwind-variants";
+
+export  const labelVariants = tv({
+    base: "text-sm text-foreground font-normal text-neutral-500 leading-4 peer-disabled:cursor-not-allowed peer-disabled:opacity-70",
+})
