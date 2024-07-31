940f0372f86235d5a7fbfde06394ab5284e4e003
Thiago <thiago.ribeiro@elo.com.br>
2024-07-31 15:50:00 -0300

    refactor: tabs components

--- packages/ui/src/components/Tabs/Tabs.stories.tsx
+++ packages/ui/src/components/Tabs/Tabs.stories.tsx
@@ -22,11 +22,11 @@ const meta = {
             <Card.Content className="space-y-2">
               <div className="space-y-1">
                 <Label htmlFor="name">Nome</Label>
-                <Input id="name" defaultValue="Paulo Mauricio" />
+                <Input id="name" />
               </div>
               <div className="space-y-1">
                 <Label htmlFor="username">Nome de usuário</Label>
-                <Input id="username" defaultValue="@paulo" />
+                <Input id="username" />
               </div>
             </Card.Content>
             <Card.Footer>
@@ -70,5 +70,61 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const TabsDemo: Story = {
-  args: {},
+  render: () => (
+    <div className="flex w-[400px]">
+      <Tabs.Root defaultValue="account" className="w-[400px]">
+        <Tabs.List className="grid w-full grid-cols-2">
+          <Tabs.Trigger value="account">Conta</Tabs.Trigger>
+          <Tabs.Trigger value="password">Senha</Tabs.Trigger>
+        </Tabs.List>
+        <Tabs.Content value="account">
+          <Card.Root>
+            <Card.Header>
+              <Card.Title className="text-2xl">Conta</Card.Title>
+              <Card.Description>
+                Faça alterações em sua conta aqui. Clique em salvar quando
+                terminar.
+              </Card.Description>
+            </Card.Header>
+            <Card.Content className="space-y-2">
+              <div className="space-y-1">
+                <Label htmlFor="name">Nome</Label>
+                <Input id="name" />
+              </div>
+              <div className="space-y-1">
+                <Label htmlFor="username">Nome de usuário</Label>
+                <Input id="username" />
+              </div>
+            </Card.Content>
+            <Card.Footer>
+              <Button>Salvar alterações</Button>
+            </Card.Footer>
+          </Card.Root>
+        </Tabs.Content>
+        <Tabs.Content value="password">
+          <Card.Root>
+            <Card.Header>
+              <Card.Title className="text-2xl">Password</Card.Title>
+              <Card.Description>
+                Altere sua senha aqui. Após salvar, você será deslogado
+              </Card.Description>
+            </Card.Header>
+            <Card.Content className="space-y-2">
+              <div className="space-y-1">
+                <Label htmlFor="current">Senha atual</Label>
+                <Input id="current" type="password" />
+              </div>
+              <div className="space-y-1">
+                <Label htmlFor="new">Nova senha</Label>
+                <Input id="new" type="password" />
+              </div>
+            </Card.Content>
+            <Card.Footer>
+              <Button>Salvar senha</Button>
+            </Card.Footer>
+          </Card.Root>
+        </Tabs.Content>
+      </Tabs.Root>
+    </div>
+  )
 };
--- packages/ui/src/components/Tabs/Tabs.test.tsx
+++ packages/ui/src/components/Tabs/Tabs.test.tsx
@@ -1,7 +1,7 @@
+import { describe, expect, it, vi } from "vitest";
 import { render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { Tabs } from "..";
-import { vi } from "vitest";
 
 describe("Tabs", () => {
   it("should match the snapshot", () => {
