0e04a58a59becb0c522512b62dccbd1168e89ac3
Hithalo Kildaree <hithalo.kildaree@elo.com.br>
2024-08-07 17:18:21 -0300

    refactor: change switch root and refactoring variants

--- packages/ui/src/components/Switch/switch.stories.tsx
+++ packages/ui/src/components/Switch/switch.stories.tsx
@@ -1,30 +1,79 @@
 import { Meta, StoryObj } from "@storybook/react";
 import { Switch } from ".";
+import { SwitchProps } from "@radix-ui/react-switch";
 
 const meta = {
   title: "Switch",
-  component: Switch.Root,
+  render: (args: any) => {
+    const { disabled } = args;
+    return (
+      <Switch.Root disabled={disabled}>
+        <Switch.Button id="1" />
+        <div className="ml-2 flex flex-col">
+          <Switch.Label htmlFor="1">Teste</Switch.Label>
+          <Switch.HelperText>Texto auxiliar ao Switch</Switch.HelperText>
+        </div>
+      </Switch.Root>
+    )
+  },
   parameters: {
     layout: "centered",
+  },
+  args: {
+    disabled: false,
   },
 } satisfies Meta<{}>;
 
 export default meta;
 
-type Story = StoryObj<typeof meta>;
+type Story = StoryObj<SwitchProps>;
 
-export const SwitchDefault: Story = {
-  name: "Switch padrão",
+
+
+export const SwitchWithoutLabel: Story = {
+  name: "Switch Padrão",
   args: {},
-  render: () => (<Switch.Root />),
+  render: () => (
+    <Switch.Root>
+      <Switch.Button />
+    </Switch.Root>
+
+  ),
 };
+
+
 export const SwitchWithTitle: Story = {
   name: "Switch com título",
   args: {},
-  render: () => (<Switch.LabelText labelText="O Brasil é caramelo"/> ),
+  render: () => (
+    <Switch.Root>
+      <Switch.Button />
+      <div className="ml-2 flex flex-col">
+        <Switch.Label>O Brasil é caramelo</Switch.Label>
+      </div>
+    </Switch.Root>
+
+  ),
 };
+
+
 export const SwitchWithHelperText: Story = {
   name: "Switch com texto auxiliar",
   args: {},
-  render: () => (<Switch.LabelText labelText="Título" helperText="Texto auxiliar ao Switch"/> ),
+  render: () => (
+    <Switch.Root>
+      <Switch.Button id="1" />
+      <div className="ml-2 flex flex-col">
+        <Switch.Label htmlFor="1">O Brasil é caramelo</Switch.Label>
+        <Switch.HelperText>Texto auxiliar ao Switch</Switch.HelperText>
+      </div>
+    </Switch.Root>
+  ),
 };
+
+export const SwitchButton: Story = {
+  args: {
+    disabled: false,
+  },
+};
+
--- packages/ui/src/components/Switch/variants.ts
+++ packages/ui/src/components/Switch/variants.ts
@@ -1,14 +1,19 @@
 import { tv } from "tailwind-variants";
 
-export const switchVariants = tv({
+const switchVariants = tv({
   slots: {
-    rootVariants: [
+    rootStyle: "flex items-center [&[aria-disabled='true']]:cursor-not-allowed [&[aria-disabled='true']]:opacity-50",
+    switchButtonStyle: [
       "peer inline-flex h-5 w-9 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent shadow-sm transition-colors",
       "data-[state=checked]:bg-green-dark-500 data-[state=unchecked]:bg-neutral-400",
       "focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background",
       "disabled:cursor-not-allowed disabled:opacity-50",
     ],
-    thumbVariants:
+    labelTextStyle: "text-sm font-medium text-green-dark-500",
+    helperTextStyle: "text-sm text-neutral-500",
+    thumbStyle:
       "pointer-events-none block h-4 w-4 rounded-full bg-background shadow-lg ring-0 transition-transform data-[state=checked]:translate-x-4 data-[state=unchecked]:translate-x-0",
   },
 });
+
+export const { rootStyle, labelTextStyle, thumbStyle, helperTextStyle, switchButtonStyle } = switchVariants();
